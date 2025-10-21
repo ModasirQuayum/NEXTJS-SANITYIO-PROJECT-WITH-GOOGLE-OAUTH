0618e1ec42be351a91943609f70868f9a43cd3f5
ModasirQuayum <67706493+ModasirQuayum@users.noreply.github.com>
2025-10-21 22:26:54 +0600

    Update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -13,7 +13,7 @@ const NavBar = async() => {
     {session
     ?
         <div className="flex items-center justify-end my-5 px-5 gap-3">
-            <div className='flex items-center flex-wrap'>
+            <div className='flex items-center flex-wrap gap-3'>
                 <div className='flex items-center gap-2'>
                     <Image className='rounded-full' src={session?.user?.image || "https://images.unsplash.com/photo-1728577740843-5f29c7586afe?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=880"} alt={session?.user?.name || "username"} width={25} />
                     <span className='text-stone-500 font-bold'>{session?.user?.name}</span>
