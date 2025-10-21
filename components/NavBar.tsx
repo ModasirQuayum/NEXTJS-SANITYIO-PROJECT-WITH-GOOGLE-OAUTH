8604cc2634416038c7af2f211f77c1c479cfbdf2
modasir-quayum <muddasirquayum@gmail.com>
2025-10-21 19:37:33 +0600

    bug fixes

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { SignOut } from './signout-button'
 import { SignIn } from './signin-button'
 import Link from 'next/link'
+import Image from 'next/image'
 
 const NavBar = async() => {
     const session = await auth()
@@ -14,7 +15,7 @@ const NavBar = async() => {
         <div className="flex items-center justify-end my-5 px-5 gap-3">
         
         <div className='flex items-center gap-2'>
-            <img className='rounded-full' src={session?.user?.image} alt={session?.user?.name} width={25} />
+            <Image className='rounded-full' src={session?.user?.image || "https://images.unsplash.com/photo-1728577740843-5f29c7586afe?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=880"} alt={session?.user?.name || "username"} width={25} />
             <span className='text-stone-500 font-bold'>{session?.user?.name}</span>
         </div>
         <Link className='text-stone-700 hover:text-stone-600' href="/content/create">Create</Link>
